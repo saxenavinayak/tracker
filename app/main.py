8a75b171bd87648245f5cf5fbaba6d5ca89bc71a
Vinayak Saxena <saxenavinayak12@gmail.com>
2026-03-19 21:32:51 -0400

    nuke poetry

--- app/main.py
+++ app/main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 
 app = FastAPI()
 
 @app.get("/")
 async def root(request: Request):
+
     requestor_ip = request.client.host
     headers = dict(request.headers)
 
     user_agent = request.headers.get("user-agent")
     print(requestor_ip, headers, user_agent)
-    return RedirectResponse(url="https://github.com/saxenavinayak")
+    
+    content = """
+    <html>
+        <head>
+            <meta http-equiv="refresh" content="3;url=https://github.com/saxenavinayak" />
+        </head>
+        <body>
+            <h1>Hello from Waterloo!</h1>
+            <p> Redirecting in 3 seconds...</p>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=content)
+
+
