0b48dd3b64973a09eee40e358230b75fad975e8a
Vinayak Saxena <saxenavinayak12@gmail.com>
2026-03-22 17:14:59 -0400

    postgres

--- app/db_setup.py
+++ app/db_setup.py
@@ -1,14 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# This creates a local file named 'test.db' in your project folder
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+POSTGRES_PW = os.getenv("POSTGRES_PW")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PW}@{POSTGRES_HOST}:{POSTGRES_PORT}/postgres"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# SQLite specific: 'check_same_thread' is needed because FastAPI is async
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+
+# # This creates a local file named 'test.db' in your project folder
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+# # SQLite specific: 'check_same_thread' is needed because FastAPI is async
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
